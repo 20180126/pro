c0bb2f995f0b64b89b7d01ea980deb38f8b6ca20
unknown <jnnua7897@gmail.com>
2018-01-28 14:45:31 +0900

    Migrationファイルを作成

---
+++ Migrations/20180126053510_CreateItems.php
@@ -0,0 +1,33 @@
+<?php
+use Migrations\AbstractMigration;
+
+class CreateItems extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('items');
+        $table->addColumn('name', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('price', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->addColumn('count', 'string', [
+            'default' => null,
+            'limit' => 255,
+            'null' => false,
+        ]);
+        $table->create();
+    }
+}
